c969d2e1a47361baef245dcd1bd5b15a304a37ee
Vince-arch <vincekimani006@gmail.com>
2023-01-01 13:11:38 +0300

    changed the naming of the nabar

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 export {default as Footer} from './Footer';
 export {default as Layout} from './Layout';
-export {default as NavBar} from './NavBar';
+export {default as Navbar} from './Navbar';
 export {default as Product} from './Product';
 export {default as HeroBanner} from './HeroBanner';
 export {default as FooterBanner} from './FooterBanner';
